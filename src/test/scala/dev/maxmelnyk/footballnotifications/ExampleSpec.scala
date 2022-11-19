392087fb65e250974e00fc4fde218e37e6d128d9
Max Melnyk <max@maxmelnyk.dev>
2022-11-18 20:07:29 -0500

    add assembly

---
+++ src/test/scala/dev/maxmelnyk/footballnotifications/ExampleSpec.scala
@@ -0,0 +1,9 @@
+package dev.maxmelnyk.footballnotifications
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ExampleSpec extends AnyFlatSpec {
+  "Example" should "succeed" in {
+    succeed
+  }
+}
