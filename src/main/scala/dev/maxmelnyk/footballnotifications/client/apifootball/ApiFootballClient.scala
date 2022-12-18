7006b0366697cc2a2cf206e439239773752e6f15
Max Melnyk <max@maxmelnyk.dev>
2022-12-17 20:15:52 -0500

    refactor a bit

--- src/main/scala/dev/maxmelnyk/footballnotifications/client/apifootball/ApiFootballClient.scala
+++ src/main/scala/dev/maxmelnyk/footballnotifications/client/apifootball/ApiFootballClient.scala
@@ -37,8 +37,8 @@ private class DefaultApiFootballClient[F[_] : Monad](sttpBackend: SttpBackend[F,
 
   def getTeamById(id: Int): F[Option[Team]] = {
     request[Seq[TeamWithVenue]](uri"$baseUrl/teams?id=$id").map {
-      case Seq(teamsWithVenue) => Some(teamsWithVenue.team)
       case Seq() => None
+      case Seq(teamsWithVenue) => Some(teamsWithVenue.team)
       case _ => throw new Exception(s"More than one team found for id $id")
     }
   }
@@ -48,21 +48,17 @@ private class DefaultApiFootballClient[F[_] : Monad](sttpBackend: SttpBackend[F,
       .get(uri)
       .header(authHeader, Config.apiFootballApiKey)
       .mapResponse {
-        case Right(responseBodyStr) =>
-          decode[Response[R]](responseBodyStr)(Response.responseDecoder) match {
-            case Right(response) =>
-              response.getErrorsMessage.foreach { error =>
-                logger.info(error)
-              }
-              response.response
-            case Left(error) =>
-              logger.error(s"Failed to parse response from API Football: $error")
-              throw new Exception(s"Failed to decode response body: $responseBodyStr", error)
-          }
         case Left(responseBodyStr) =>
           decode[ErrorResponse](responseBodyStr) match {
-            case Right(errorResponse) => throw new Exception(errorResponse.message)
             case Left(error) => throw new Exception(s"Failed to decode error response body: $responseBodyStr", error)
+            case Right(errorResponse) => throw new Exception(errorResponse.message)
+          }
+        case Right(responseBodyStr) =>
+          decode[Response[R]](responseBodyStr)(Response.responseDecoder) match {
+            case Left(error) => throw new Exception(s"Failed to decode response body: $responseBodyStr", error)
+            case Right(response) =>
+              response.getErrorsMessage.foreach(logger.info(_: String))
+              response.response
           }
       }
       .send(sttpBackend)
